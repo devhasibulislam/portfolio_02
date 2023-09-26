d47f047ca46a437f9cf48bdd56984965bb6d5697
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-26 09:43:46 +0200

    try edge for pages only

--- app/projects/layout.jsx
+++ app/projects/layout.jsx
@@ -1,5 +1,3 @@
-export const runtime = 'edge';
-
 export default function ProjectsLayout({ children }) {
 	return (
 		<div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900">
